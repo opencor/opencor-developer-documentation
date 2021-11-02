91a816f3cf3ee21dcdac7b6b3aeda776497dd71a
Alan Garny <agarny@hellix.com>
2021-11-03 10:29:06 +1300

    Use the Sphinx sphinx_copybutton and sphinx_inline_tabs extensions.

--- src/conf.py
+++ src/conf.py
@@ -28,6 +28,8 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_copybutton',
+    'sphinx_inline_tabs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
